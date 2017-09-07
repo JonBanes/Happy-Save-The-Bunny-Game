feea81f76ec08ecd06eaf5d2ad4bb87aef9db737
JonBanes <ADSteinberg@gmail.com>
2017-09-07 13:51:04 -0600

    including 'game' module
    changes to make the 'game' module work with the main program

--- main_game.py
+++ main_game.py
@@ -13,66 +13,39 @@ import game
     
 def main():
     """ Main function for the game. """
+    # initialize pygame
     pygame.init()
  
     # Set the width and height of the screen [width,height]
-    size = [1200, 900]
+    size = [constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT]
     screen = pygame.display.set_mode(size)
  
-    pygame.display.set_caption("My Game")
+    pygame.display.set_caption("Happy Save The Bunny Game")
  
     # Loop until the user clicks the close button.
     done = False
  
-    # Used to manage how fast the screen updates
+    # used to manage how fast the screen updates
     clock = pygame.time.Clock()
     
     # Build UI objects
-    test_screen = user_interface.UserInterface()
+    test_game = game.Game()
     
     # -------- Main Program Loop -----------
     while not done:
-        # ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-                
-        # ALL EVENT PROCESSING SHOULD GO ABOVE THIS COMMENT
- 
-        # ALL GAME LOGIC SHOULD GO BELOW THIS COMMENT
+        # event processing
+        done = test_game.process_events()
         
-        # ALL GAME LOGIC SHOULD GO ABOVE THIS COMMENT
- 
-        # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
- 
-        # First, clear the screen to white. Don't put other drawing commands
-        # above this, or they will be erased with this command.
+        # Game logic
+        test_game.run_logic()
         
-        # get the mouse position in the loop
-        test_screen.mouse_x = pygame.mouse.get_pos()[0]
-        test_screen.mouse_y = pygame.mouse.get_pos()[1]
-        
-        if test_screen.whole_map_blitted == False:
-            test_screen.draw_whole_terrain_grid(screen)
-            
-        #test_screen.draw_changed_tiles(screen)
-        
-        test_screen.set_cursor_sprite(screen)
-        test_screen.cursor_sprite = test_screen.cursor_land
-        
-        
-        
-        # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
- 
-        # Go ahead and update the screen with what we've drawn.
-        pygame.display.flip()
+        # Draw the frame
+        test_game.display_frame(screen)
  
         # Limit to 60 frames per second
         clock.tick(60)
  
     # Close the window and quit.
-    # If you forget this line, the program will 'hang'
-    # on exit if running from IDLE.
     pygame.quit()
  
 if __name__ == "__main__":
